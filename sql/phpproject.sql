f64ae9e30c30ff3a394d3296d3cb12ece9bf1edc
olympia019 <vincent_vanloock@hotmail.com>
2015-04-02 11:18:28 +0200

    databank update3

--- sql/phpproject.sql
+++ sql/phpproject.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Genereertijd: 02 apr 2015 om 10:47
+-- Genereertijd: 02 apr 2015 om 11:18
 -- Serverversie: 5.6.11
 -- PHP-versie: 5.5.3
 
@@ -65,7 +65,8 @@ CREATE TABLE IF NOT EXISTS `tblbuddies` (
   `buddieEmail` varchar(250) NOT NULL,
   `buddieTwitter` varchar(250) NOT NULL,
   `buddiePassword` varchar(250) NOT NULL,
-  `richtingID` int(11) NOT NULL,
+  `buddieRichting` int(11) NOT NULL,
+  `buddieJaar` int(11) NOT NULL,
   `buddieRating` int(11) NOT NULL,
   `buddieFoto` varchar(256) NOT NULL,
   PRIMARY KEY (`buddieID`)
@@ -81,19 +82,6 @@ CREATE TABLE IF NOT EXISTS `tbldatums` (
   `datumID` int(11) NOT NULL AUTO_INCREMENT,
   `datumDate` date NOT NULL,
   PRIMARY KEY (`datumID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `tblrichting`
---
-
-CREATE TABLE IF NOT EXISTS `tblrichting` (
-  `richtingID` int(11) NOT NULL AUTO_INCREMENT,
-  `richtingNaam` int(11) NOT NULL,
-  `richtingJaar` int(11) NOT NULL,
-  PRIMARY KEY (`richtingID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
