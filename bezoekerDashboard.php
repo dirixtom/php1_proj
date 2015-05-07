d0396795cc0057d071da2475e470b383eb450f52
Noë Baeten <noe.baeten@gmail.com>
2015-05-07 18:43:05 +0200

    Test 2

--- bezoekerDashboard.php
+++ bezoekerDashboard.php
@@ -5,18 +5,12 @@
 	include_once("classes/Datum.class.php");
 	include_once("classes/Student.class.php");
 	include_once("classes/Boeking.class.php");
-
 	$d = new Datum();
 	$data = $d->ShowDate();
-
 	$s = new Student();
-
 	$allStudents = $s->GetAllStudents();
-
 	$allFB = $s->GetAllFB();
-
 	$b = new Boeking();
-
 	if($_POST)
 	{
 		try 
@@ -25,96 +19,14 @@
 			$b->Datum = $_POST['datum'];
 			$b->Buddy = $_POST['buddieID'];
 			$b->Student = $_POST['studentID'];
-
 			$b->nieuweBoeking();
-
 			$success = "Uw boeking is gelukt!";
-
 		}
 		catch(Exception $e)
 		{
 			$error = $e->getMessage();
 		}
-
-	$aStudents = $s->GetAllStudents();
-
-	$b = new Boeking();
-	$allBookings = $b->getAllBoekingen();
-
-	if(!empty($_POST)) {
-		try {
-			
-			$b->datumID = $_POST['datum'];
-			$b->studentID = $_POST['studentID'];
-			$b->buddieID = $_POST['student_radio_id'];
-			
-			$b->nieuweBoeking();
-		
-		} catch( Exception $e) {
-			$error = $e->getMessage();
-		}
-		
 	}
-
-//session var is still there
-$app_id				= '1378145582515326';  //localhost
-$app_secret 		= '2819b5faff3c55c4808ed979975eb46d';
-$required_scope 	= 'public_profile, publish_actions'; //Permissions required
-$redirect_url 		= 'http://localhost:8888/PHP1/php1_proj/bezoekerDashboard.php'; //FB redirects to this page with a code
-
-//MySqli details for saving user details
-$mysql_host			= 'localhost';
-$mysql_username		= 'root';
-$mysql_password		= 'root';
-$mysql_db_name		= 'phpproject';
-
-require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/autoload.php"; //include autoload from SDK folder
-
-//import required class to the current scope
-use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRedirectLoginHelper;
-
-FacebookSession::setDefaultApplication($app_id , $app_secret);
-$helper = new FacebookRedirectLoginHelper($redirect_url);
-
-try {
-  $session = $helper->getSessionFromRedirect();
-} catch(FacebookRequestException $ex) {
-	die(" Error : " . $ex->getMessage());
-} catch(\Exception $ex) {
-	die(" Error : " . $ex->getMessage());
-}
-
-
-//if user wants to log out
-if(isset($_GET["log-out"]) && $_GET["log-out"]==1){
-	unset($_SESSION["fb_user_details"]);
-	//session ver is set, redirect user 
-	header("location: ". $redirect_url);
-}
-
-//Test normal login / logout with session
-
-if ($session){ //if we have the FB session
-	//get user data
-	$user_profile = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
-	
-	//save session var as array
-	$_SESSION["fb_user_details"] = $user_profile->asArray(); 
-	
-	$user_id = ( isset( $_SESSION["fb_user_details"]["id"] ) )? $_SESSION["fb_user_details"]["id"] : "";
-	$user_name = ( isset( $_SESSION["fb_user_details"]["name"] ) )? $_SESSION["fb_user_details"]["name"] : "";
-	$user_email = ( isset( $_SESSION["fb_user_details"]["email"] ) )? $_SESSION["fb_user_details"]["email"] : "";
-	
-	###### connect to user table ########
-	$mysqli = new mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_db_name);
-	if ($mysqli->connect_error) {
-		die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
->>>>>>> origin/master
-	}
-
 	//FACEBOOK LOGIN - NIET AANKOMEN
 	
 	//session var is still there
@@ -122,24 +34,19 @@ if ($session){ //if we have the FB session
 	$app_secret 		= '2819b5faff3c55c4808ed979975eb46d';
 	$required_scope 	= 'public_profile, publish_actions'; //Permissions required
 	$redirect_url 		= 'http://localhost:8888/PHP1/php1_proj/bezoekerDashboard.php'; //FB redirects to this page with a code
-
 	//MySqli details for saving user details
 	$mysql_host			= 'localhost';
 	$mysql_username		= 'root';
 	$mysql_password		= 'root';
 	$mysql_db_name		= 'phpproject';
-
 	require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/autoload.php"; //include autoload from SDK folder
-
 	//import required class to the current scope
 	use Facebook\FacebookSession;
 	use Facebook\FacebookRequest;
 	use Facebook\GraphUser;
 	use Facebook\FacebookRedirectLoginHelper;
-
 	FacebookSession::setDefaultApplication($app_id , $app_secret);
 	$helper = new FacebookRedirectLoginHelper($redirect_url);
-
 	try {
 	  $session = $helper->getSessionFromRedirect();
 	} catch(FacebookRequestException $ex) {
@@ -147,24 +54,13 @@ if ($session){ //if we have the FB session
 	} catch(\Exception $ex) {
 		die(" Error : " . $ex->getMessage());
 	}
-	
-	//session ver is set, redirect user 
-	header("location: ". $redirect_url);
-	$active_user_id = $mysqli->query("SELECT id FROM usertable WHERE fbid=".$user_id);
-	return $active_user_id;
-}
-
-
-
 	//if user wants to log out
 	if(isset($_GET["log-out"]) && $_GET["log-out"]==1){
 		unset($_SESSION["fb_user_details"]);
 		//session ver is set, redirect user 
 		header("location: ". $redirect_url);
 	}
-
 	//Test normal login / logout with session
-
 	if ($session){ //if we have the FB session
 		//get user data
 		$user_profile = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
@@ -194,7 +90,6 @@ if ($session){ //if we have the FB session
 		header("location: ". $redirect_url);
 		
 	}
-
 	/*else
 	{ 	
 		if(isset($_SESSION["fb_user_details"]))
@@ -211,14 +106,12 @@ if ($session){ //if we have the FB session
 			$login_url = $helper->getLoginUrl( array( 'scope' => $required_scope ) );
 			echo '<a href="'.$login_url.'">Login with Facebook</a>'; 
 		}
-
 	/*if(!isset($_SESSION["fb_user_details"]))
 		{
 		    header("location:login.php");
 		    exit();
 		}
 	*/
-
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -229,7 +122,7 @@ if ($session){ //if we have the FB session
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<?php var_dump($active_user_id ) ?>
+	
 	<div class="navbar navbar-default">
    		<div class="navbar-header">
     		<a class="navbar-nav" href="index.php"><img class="logo" src="images/logo2.png" alt="The Rent A Student Logo" width="55%"></a>
@@ -297,7 +190,6 @@ if ($session){ //if we have the FB session
 						<?php
 							//include db configuration file
 							include_once("ajax/config.php");
-
 							//MySQL query
 							$results = $mysqli->query("SELECT * FROM tblstudenten WHERE fbid = '" . $_SESSION["fb_user_details"]["id"] . "'");
 							//get all records
@@ -305,7 +197,6 @@ if ($session){ //if we have the FB session
 							{
 							  echo '<input type="hidden" value="' . $row['studentID'] . '" name="studentID" />';
 							}
-
 						?>
 
 					
@@ -334,52 +225,6 @@ if ($session){ //if we have the FB session
 				</div>
 			</div>
 			
-					<div class="row">
-						<div class="col-md-1"></div>
-						<div class="col-md-3">
-							<label for="datum">Gelieve de gewenste datum door te geven</label>
-						</div>
-						<div class="col-md-8">
-							
-							<select required name="datum" id="datum" name="datum">
-								<option value=""> Kies je datum </option>
-								<?php foreach ($data as $d): ?>
-								<option value="<?php echo $d['Id'] ;?>"><?php echo $d['datumDag'] . " " . $d['datumMaand'] . " " . $d['datumJaar'] ;?></option>
-								<?php endforeach;?>
-							</select>
-
-							
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-1"></div>
-						<div class="col-md-3">
-							<label for="Buddy">Kies hier je gewenste buddy:</label>
-						</div>
-						<div class="col-md-8">
-							<ul>
-								<?php foreach ($aStudents as $s): ?>
-								<li class="buddy_list">
-									
-										<img src="<?php echo $s['buddieFoto'] ?>" alt="<?php echo "photo" . " " . $s['buddieNaam'];  ?>" class="center-cropped"/>
-								
-									<h3><?php echo $s['buddieVoornaam'] . " " . $s['buddieNaam']; ?></h3>
-									<h4><?php echo $s['buddieJaar'] . "e jaar " . $s['buddieRichting'];  ?></h4>
-									<input type="radio" value="<?php echo $s['buddieID'] ;?>" name="student_radio_id">
-								</li>
-								<br/>
-								<?php endforeach; ?>
-							</ul>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-1"></div>
-						<div class="col-md-11">
-							<br/><button type="submit">Boeken</button>
-						</div>
-					</div>
 			</form>
 
 		</div>
